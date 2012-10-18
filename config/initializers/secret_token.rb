abcf5490259150066722d38790db9ca39daa57b0
Eric Wong <ericsyw@gmail.com>
2012-10-18 19:08:10 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yawo::Application.config.secret_token = 'fdb2a03e68f7739b73570a439a2da41f1f638eda3add15889acd13ac61d2d578bd9b33a93d7e8f2d7ff01d46e52aa8affb5454a35d9467c54c08bf58744d7134'
